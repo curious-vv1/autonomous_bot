ecbaa6064e17c2c7f290ab85a5ddd246cbc3bb62
curious-vv1 <vivekrealc2@gmail.com>
2025-01-17 17:41:19 +0530

    lidar-gazebo and pi launch

---
+++ auto_bot/launch/rplidar.launch.py
@@ -0,0 +1,20 @@
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+
+    rplidar = Node(package="rplidar_ros",executable="rplidar_composition",output="screen",parameters=[{
+        "serial_port":"/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0",
+        "frame_id":"laser_link",
+        "amgle_compensate":"True",
+        "scan_mode":"Standard"
+    }])
+
+    # This is the launch code to be used using raspberry pi connected to lidar with path mentioned in serial port
+
+    return LaunchDescription([
+        rplidar
+    ])
