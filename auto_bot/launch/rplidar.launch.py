9bf8414fb5d640a3d3e66e8ee2eaaf38711ebd43
curious-vv1 <vivekrealc2@gmail.com>
2025-01-19 00:16:18 +0530

    camera and depth camera addition

--- auto_bot/launch/rplidar.launch.py
+++ auto_bot/launch/rplidar.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
     rplidar = Node(package="rplidar_ros",executable="rplidar_composition",output="screen",parameters=[{
         "serial_port":"/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0",
         "frame_id":"laser_link",
-        "amgle_compensate":"True",
+        "angle_compensate":"True",
         "scan_mode":"Standard"
     }])
 
